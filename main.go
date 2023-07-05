6175a0afbab9e8b79c2b3859c656e09aba4521c4
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-07-05 20:44:14 +0100

    concurrency first commit - AP

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go addPartOne(1, 2, ch1)
+	go addPartTwo(1, 2, ch2)
+	fmt.Println(<-ch1 + <-ch2)
+
+}
+
+func addPartOne(a, b int, c chan int) {
+	c <- a + b
+}
+
+func addPartTwo(a, b int, c chan int) {
+	c <- a + b
+}
