9b3f973ed3999b690913f09c365a2bcc656ce0b2
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-07-09 02:57:36 +0100

    Finally figured out the deadlock issue I was having when dealing with dynamic multi-threading -AP

--- select_and_default.go
+++ select_and_default.go
@@ -7,22 +7,26 @@ func StartSelect() {
 	ch2 := make(chan int)
 	ch3 := make(chan int)
 	ch4 := make(chan int)
-
+	chansSlice := []chan int{ch1, ch2, ch3, ch4}
+	
 	go NumtoChannel(ch1)
 	go NumtoChannel(ch2)
 	go NumtoChannel(ch3)
 	go NumtoChannel(ch4)
+	
+	// SelectReader(chansSlice)
+	SelectReaderV2(chansSlice)
 
-	chansSlice := []chan int{ch1, ch2, ch3, ch4}
-	SelectReader(chansSlice)
-	// SelectReaderV2(chansSlice)
+	
 }
 
 func NumtoChannel(ch chan int) {
 	for i := 0; i < 10; i++ {
 		ch <- i
 	}
+	close(ch)
 }
+
 
 func SelectReader(chans []chan int) {
 	nums := []int{}
@@ -36,6 +40,48 @@ func SelectReader(chans []chan int) {
 			nums = append(nums, v)
 		case v := <-chans[3]:
 			nums = append(nums, v)
+		}
+	}
+	fmt.Println(nums)
+}
+
+func SelectReaderV2(chans []chan int) {
+	nums := []int{}
+	for {
+		select {
+			case v, ok := <-chans[0]:
+				if ok != false{
+					fmt.Println(v)
+					nums = append(nums, v)
+					
+				}else{
+					chans[0] = nil
+					// nums = append(nums, v)
+				}
+			case v, ok := <-chans[1]:
+				if ok != false{
+					nums = append(nums, v)
+				}else{
+					chans[1] = nil
+					// nums = append(nums, v)
+				}
+			case v, ok := <-chans[2]:
+				if ok != false{
+					nums = append(nums, v)
+				}else{
+					chans[2] = nil
+					// nums = append(nums, v)
+				}
+			case v, ok := <-chans[3]:
+				if ok != false{
+					nums = append(nums, v)
+				}else{
+					chans[3] = nil
+					// nums = append(nums, v)
+				}
+		}
+		if chans[0] == nil && chans[1] == nil && chans[2] == nil && chans[3] == nil{
+			break
 		}
 	}
 	fmt.Println(nums)
