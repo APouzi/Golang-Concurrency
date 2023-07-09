9404e4110ac4e92508a75796e13ebc8258fbbf7c
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-07-09 03:02:19 +0100

    Refactoring the selection to run both dynamic and non dynamic and removing print statement -AP

--- select_and_default.go
+++ select_and_default.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
-func StartSelect() {
+func SelectInit(){
+	fmt.Println("\n Select Start \n")
+	StartSelectWithoutClose()
+	StartSelectWithClose()
+}
+
+func StartSelectWithoutClose() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 	ch3 := make(chan int)
@@ -14,20 +20,41 @@ func StartSelect() {
 	go NumtoChannel(ch3)
 	go NumtoChannel(ch4)
 	
-	// SelectReader(chansSlice)
-	SelectReaderV2(chansSlice)
-
-	
+	fmt.Println("\n Select with numbered loop and no close\n")
+	SelectReader(chansSlice)
 }
+
 
 func NumtoChannel(ch chan int) {
 	for i := 0; i < 10; i++ {
 		ch <- i
 	}
-	close(ch)
+}
+
+func StartSelectWithClose() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+	ch4 := make(chan int)
+	chansSlice := []chan int{ch1, ch2, ch3, ch4}
+	
+	go NumtoChannelWithClose(ch1)
+	go NumtoChannelWithClose(ch2)
+	go NumtoChannelWithClose(ch3)
+	go NumtoChannelWithClose(ch4)
+	
+	fmt.Println("\n Select with inifite loop and close, making it dynamic\n")
+	SelectReaderV2(chansSlice)
+	
 }
 
 
+func NumtoChannelWithClose(ch chan int) {
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	close(ch)
+}
 func SelectReader(chans []chan int) {
 	nums := []int{}
 	for i := 0; i < 40; i++ {
@@ -51,33 +78,27 @@ func SelectReaderV2(chans []chan int) {
 		select {
 			case v, ok := <-chans[0]:
 				if ok != false{
-					fmt.Println(v)
 					nums = append(nums, v)
-					
 				}else{
 					chans[0] = nil
-					// nums = append(nums, v)
 				}
 			case v, ok := <-chans[1]:
 				if ok != false{
 					nums = append(nums, v)
 				}else{
 					chans[1] = nil
-					// nums = append(nums, v)
 				}
 			case v, ok := <-chans[2]:
 				if ok != false{
 					nums = append(nums, v)
 				}else{
 					chans[2] = nil
-					// nums = append(nums, v)
 				}
 			case v, ok := <-chans[3]:
 				if ok != false{
 					nums = append(nums, v)
 				}else{
 					chans[3] = nil
-					// nums = append(nums, v)
 				}
 		}
 		if chans[0] == nil && chans[1] == nil && chans[2] == nil && chans[3] == nil{
